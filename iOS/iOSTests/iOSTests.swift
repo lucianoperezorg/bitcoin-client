76dbdcdda79e44b92c79cdf832f622e908dd1b67
Luciano Perez <lucianopereztiranti@gmail.com>
2022-12-04 12:52:56 +0000

    Initial init

--- iOS/iOSTests/iOSTests.swift
+++ iOS/iOSTests/iOSTests.swift
@@ -1,14 +1,14 @@
 //
-//  Bitcoin_iOSTests.swift
-//  Bitcoin-iOSTests
+//  iOSTests.swift
+//  iOSTests
 //
-//  Created by Luciano Perez on 03/12/2022.
+//  Created by Luciano Perez on 04/12/2022.
 //
 
 import XCTest
-@testable import Bitcoin_iOS
+@testable import iOS
 
-final class Bitcoin_iOSTests: XCTestCase {
+final class iOSTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
